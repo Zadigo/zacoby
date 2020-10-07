db4a6be3b0d56ead849ddd8f3cfefc6f66860d7e
John Pendenque <pendenquejohn@gmail.com>
2020-10-07 16:06:20 +0200

    Find elements

--- service/commands.py
+++ service/commands.py
@@ -11,11 +11,14 @@ class BrowserCommands(Enum):
 
     FIND_ELEMENT = ['findElement', ['POST', '/session/$sessionId/element']]
     FIND_ELEMENTS = ['findElement', ['POST', '/session/$sessionId/elements']]
+    FIND_CHILD_ELEMENT = ['findChildElement', ['POST', '/session/$sessionId/element/$id/element']]
+    FIND_CHILD_ELEMENTS = ['findChildElements', ['POST', '/session/$sessionId/element/$id/elements']]
     
     GET = ['get', ['POST', '/session/$sessionId/url']]
     GET_TITLE = ['getTitle', ['GET', '/session/$sessionId/title']]
     GET_PAGE_SOURCE = ['getPageSource', ['GET', '/session/$sessionId/source']]
-    GET_ELEMENT_TAG_NAME = []
+    GET_ELEMENT_TAG_NAME = ['getElementTagName', ['GET', '/session/$sessionId/element/$id/name']]
+    GET_CURRENT_URL = ['getCurrentUrl', ['GET', '/session/$sessionId/url']]
 
     NEW_SESSION = ['session', ['POST', '/session']]
     
